bbb6546d7465912ae9b1555a582035381eccd87a
miguelrs77 <miguelr.s77@gmail.com>
2024-05-25 17:15:36 -0300

    Primer ejercicio de ISPC

---
+++ programacion/promedioNotas.py
@@ -0,0 +1,9 @@
+nota1 = float(input("Ingrese la nota 1: "))
+nota2 = float(input("Ingrese la nota 2: "))
+nota3 = float(input("Ingrese la nota 3: "))
+nota4 = float(input("Ingrese la nota 4: "))
+
+promedio = (nota1+nota2+nota3+nota4)/49
+
+
+print(f'El promedio es: {promedio}')

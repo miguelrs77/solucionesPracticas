0a198aa24eb7c107ebf7e08075535019e6ef7a41
miguelrs77 <miguelr.s77@gmail.com>
2024-05-25 21:06:01 -0300

    segundo ejercicio

---
+++ programacion/mayorEdad.py
@@ -0,0 +1,8 @@
+edad = int(input("Ingrese su edad: "))
+
+if edad > 18:
+    print(f'La persona es mayor de edad tiene: {edad} años')
+    #//if edad >40:
+        #print(f'La persona es mayor de 40 años de edad, tiene {edad} años')
+else:
+        print(f'Lapersona es menor de edad tiene: {edad} años')    
